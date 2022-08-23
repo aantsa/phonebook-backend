cce9fe7c186e9edd4dd6a27e6f050fc9d98568a1
Anton Fagerholm <anton.fagerholm@gmail.com>
2022-08-23 19:58:30 +0300

    changes for heroku deployment

---
+++ build/precache-manifest.dbd158fd129bf41048a779b365af7130.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a029a8ae30b5dd3d1a740089e269530e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c01945bcdbff4223ece3",
+    "url": "/static/css/main.db3a6d0b.chunk.css"
+  },
+  {
+    "revision": "3bb190c8cf2f9f412ffc",
+    "url": "/static/js/2.33e09fe1.chunk.js"
+  },
+  {
+    "revision": "15f0bb1ad53358fb251f081b1ea57be7",
+    "url": "/static/js/2.33e09fe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c01945bcdbff4223ece3",
+    "url": "/static/js/main.cc8e3d38.chunk.js"
+  },
+  {
+    "revision": "7180cc4f3c947c11ba02",
+    "url": "/static/js/runtime-main.a59fafa6.js"
+  }
+]);
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const cors = require('cors')
+app.use(express.static('build'))
+
+app.use(cors())
 
 app.use(express.json());
 
@@ -84,6 +88,7 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
